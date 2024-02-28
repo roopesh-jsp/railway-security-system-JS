2b7b536296dea48ef1590b49608f53567819ad77
roopesh-jsp <rupzkumar5@gmail.com>
2024-02-28 13:23:36 +0530

    html

--- script.js
+++ script.js
@@ -2,11 +2,16 @@
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar");
 const navBarLinks = document.querySelectorAll(".navLink");
-hamburger.addEventListener("click", () => {
-  navbar.classList.toggle("navOpen");
-  navBarLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      navbar.classList.remove("navOpen");
+const loginbtn = document.querySelector(".loginbtn");
+const usernameInput = document.querySelector(".username-input");
+let username = "";
+if (hamburger) {
+  hamburger.addEventListener("click", () => {
+    navbar.classList.toggle("navOpen");
+    navBarLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        navbar.classList.remove("navOpen");
+      });
     });
   });
-});
+}
